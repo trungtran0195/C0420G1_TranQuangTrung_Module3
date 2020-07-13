5ab093b3342c95ff085dbc96d04b8af194a64f6a
trungtran <trantrung0195@gmail.com>
2020-07-13 16:02:36 +0700

    [Bài tập] Thực thi các câu lệnh SQL

---
+++ [Bài tập] Thực thi các câu lệnh SQL.sql
@@ -0,0 +1,6 @@
+SELECT * FROM customer
+SELECT customerName, phone, city, country FROM customers 
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique'
+SELECT * FROM customers WHERE customername like '%A%'
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC') 
+
